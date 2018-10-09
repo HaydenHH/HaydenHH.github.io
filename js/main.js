f87d3dd27f69b92bb0a59a17258a739485c2facc
Liu Yan <>
2018-10-09 02:16:11 +0100

    Site updated: 2018-10-09 02:16:11

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 // hhy client-side javascript
 window.onload = function(){
 	// alert("1")
+	
+
+	$(".recent-post-item").hover(function() {
+		$(this).css('background','#F2F3F3')
+		$(".recent-post-title").hover(function() {
+		$(this).parent('.recent-post-info').parent('.recent-post-item').css({
+			'background':'#ddd',
+			'transition':'0.3s ease-out'
+	})
+		
+	}, function() {
+		$(this).parent('.recent-post-info').parent('.recent-post-item').css({
+			'background':'#F2F3F3',
+			'transition':'0.78s'
+	})
+	});
+	}, function() {
+		$(this).css('background','white')
+	});
+	
+	
 }
